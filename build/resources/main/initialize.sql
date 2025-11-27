82dabc9c879b3d9bc52b2494f2d3dd04e99b8037
kdh1123 <164202950+kdh1123@users.noreply.github.com>
2025-11-27 13:38:35 +0900

    backup: save in-progress test fixes

---
+++ build/resources/main/initialize.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, name, email) VALUES(1, '김철수', 'chs@gmail.com');
+INSERT INTO users (id, name, email) VALUES(2, '신짱구', 'jj9@gmail.com');
+INSERT INTO users (id, name, email) VALUES(3, '맹구', 'mg@gmail.com');
--- src/test/java/com/dgsw/project2/controller/ItemCommentControllerTest.java
+++ src/test/java/com/dgsw/project2/controller/ItemCommentControllerTest.java
@@ -31,13 +31,7 @@ public class ItemCommentControllerTest {
     @Test
     @DisplayName("댓글 생성 테스트")
     void createCommentTest() throws Exception {
-        String json = """
-            {
-                "itemId": 1,
-                "authorId": 1,
-                "content": "댓글 내용"
-            }
-        """;
+        String json = "{\"itemId\":1,\"authorId\":1,\"content\":\"댓글 내용\"}";
 
         ItemCommentResponse response = new ItemCommentResponse(1L, 1L, 1L, "댓글 내용");
         given(itemCommentService.create(new CreateItemCommentRequest(1L,1L,"댓글 내용"))).willReturn(response);
@@ -78,11 +72,7 @@ public class ItemCommentControllerTest {
     @Test
     @DisplayName("댓글 수정 테스트")
     void updateCommentTest() throws Exception {
-        String json = """
-            {
-                "content": "댓글1-수정"
-            }
-        """;
+        String json = "{\"content\":\"댓글1-수정\"}";
 
         ItemCommentResponse response = new ItemCommentResponse(1L,1L,1L,"댓글1-수정");
         given(itemCommentService.update(1L, new UpdateItemCommentRequest("댓글1-수정")))
@@ -104,3 +94,46 @@ public class ItemCommentControllerTest {
                 .andExpect(status().isOk());
     }
 }
+
+
+            @Test
+            @DisplayName("단일 댓글 조회 테스트")
+            void getCommentByIdTest() throws Exception {
+                ItemCommentResponse response = new ItemCommentResponse(1L,1L,1L,"댓글1");
+                given(itemCommentService.findById(1L)).willReturn(response);
+
+                mockMvc.perform(get("/item-comments/1"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.id").value(1))
+                        .andExpect(jsonPath("$.content").value("댓글1"));
+            }
+
+            @Test
+            @DisplayName("댓글 수정 테스트")
+            void updateCommentTest() throws Exception {
+                String json = """
+                    {
+                        "content": "댓글1-수정"
+                    }
+                """;
+
+                ItemCommentResponse response = new ItemCommentResponse(1L,1L,1L,"댓글1-수정");
+                given(itemCommentService.update(1L, new UpdateItemCommentRequest("댓글1-수정")))
+                        .willReturn(response);
+
+                mockMvc.perform(put("/item-comments/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.content").value("댓글1-수정"));
+            }
+
+            @Test
+            @DisplayName("댓글 삭제 테스트")
+            void deleteCommentTest() throws Exception {
+                willDoNothing().given(itemCommentService).delete(1L);
+
+                mockMvc.perform(delete("/item-comments/1"))
+                        .andExpect(status().isOk());
+            }
+        }
